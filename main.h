e8ee306086c3d1197a441f7951b61e38decae4a6
JamesonJc <jamesonpillay@gmail.com>
2023-06-27 16:51:08 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Libraries*/
+#include <unistd.h>
+#include <stdarg.h>
+
+
+/*Function prototypes*/
+int _puts(char *str);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
